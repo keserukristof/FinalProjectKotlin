a7bfb17a7302603a925eb13b8dd00883cc7c8528
keserukristof <41549423+keserukristof@users.noreply.github.com>
2019-12-12 15:54:54 +0100

    Author ismét

--- app/src/main/java/com/example/finalprojectkotlin/Author.java
+++
@@ -1,4 +0,0 @@
-package com.example.finalprojectkotlin;
-
-public class Author {
-}
---
+++ app/src/main/java/com/example/finalprojectkotlin/model/Author.kt
@@ -0,0 +1,7 @@
+package com.example.finalprojectkotlin.model
+
+class Author {
+    lateinit var name : String
+
+    lateinit var neptun : String
+}
