5c2d9fb91dcc7156e16d288f5a16e272625da823
keserukristof <41549423+keserukristof@users.noreply.github.com>
2019-12-12 15:52:56 +0100

    Author

---
+++ app/src/main/java/com/example/finalprojectkotlin/Author.java
@@ -0,0 +1,4 @@
+package com.example.finalprojectkotlin;
+
+public class Author {
+}
--- app/src/main/java/com/example/finalprojectkotlin/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectkotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.finalprojectkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalprojectkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //Create binding object
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
 }
